284d1c325b33a39e85ce32fdb2aaee57b16ddfd6
Vadim Tomnikov <vadim.tomnikov@sap.com>
2014-09-21 16:10:43 +0300

    index.html with dummy images added. Angular installed

---
+++ public/scripts/app.js
@@ -0,0 +1,26 @@
+angular.module('app',[]).
+    controller('ctrl', function($scope) {
+        $scope.images = [{
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }, {
+            url: 'http://lorempixel.com/200/200?ts=' + Math.random()
+        }];
+    });
